dbcf9ab76a40fc524fac98a1ebbb2ea072fe3d5e
hayaa1234 <hayalawansah@gmail.com>
2022-03-10 15:41:40 +0300

    adding new events

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
-
+    'events',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
@@ -169,3 +169,11 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+
+# JOYOUS_THEME_CSS = "/static/joyous/css/joyous_forest_theme.css"
+# JOYOUS_HOLIDAYS = "Scotland"
+# JOYOUS_DATE_FORMAT = "l jS \\o\\f F X"
+# JOYOUS_DATE_SHORT_FORMAT = "j M X"
+# JOYOUS_TIME_FORMAT = "fq"
+# JOYOUS_TIME_INPUT = "12"
